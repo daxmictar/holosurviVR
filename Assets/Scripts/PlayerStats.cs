0c15755f711c0420a0f39be96752813c480dfdfe
noahmagalei <magalei.np@gmail.com>
2024-05-05 14:01:11 -0700

    added audio sources

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -8,6 +9,32 @@ public class PlayerStats : MonoBehaviour
     public int experienceToLevel = 100;
     public int level = 1;
     public bool levelingUp = false;
+
+    public AudioClip hitSound;
+    public Image damageIndicator;
+    private AudioSource audioSource;
+    private float indicatorDuration = 2.0f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        if (damageIndicator != null)
+        {
+            damageIndicator.gameObject.SetActive(false);
+            
+            // Ensure damage indicator is a child of a Canvas set to Screen Space - Overlay
+            Canvas canvas = damageIndicator.GetComponentInParent<Canvas>();
+            if (canvas.renderMode != RenderMode.ScreenSpaceOverlay)
+            {
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            }
+        }
+    }
 
     public void Update()
     {
@@ -36,5 +63,42 @@ public class PlayerStats : MonoBehaviour
         GameObject ui = GameObject.FindGameObjectWithTag("UICanvas");
         ScreenOverlay screenOverlay = ui.GetComponent<ScreenOverlay>();
         screenOverlay.GeneratePowerups();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        ShowDamageIndicator();
+        PlayHitSound();
+    }
+
+    private void ShowDamageIndicator()
+    {
+        if (damageIndicator != null)
+        {
+            damageIndicator.gameObject.SetActive(true);
+            Invoke("HideDamageIndicator", indicatorDuration);
+        }
+        else
+        {
+            Debug.LogWarning("Damage indicator is not assigned.");
+        }
+    }
+
+    private void HideDamageIndicator()
+    {
+        if (damageIndicator != null)
+        {
+            Debug.Log("Hiding damage indicator");
+            damageIndicator.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlayHitSound()
+    {
+        if (hitSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
     }
 }
